924de60fb3eb924e9fd4d950c58ae74f7b4f5040
doolb <xue.blood@outlook.com>
2018-04-12 17:14:16 +0800

    add usage help

--- alk.sh
+++ alk.sh
@@ -46,8 +46,27 @@ function link(){
   ln -s $1 $tgt
 }
 
+function usage(){
+  echo ' 
+alk
+	Auto Link tool with 'index' file
+Usage:
+========================
+|    alk SOURCE TARGET |
+========================
 
-if test $# != 2;then echo error; exit -1;fi;
+	`SOURCE`: The root path for you want to link from
+	`TARGET`: The path you want to link to
+	!Both `SOURCE` and `TARGET` dir should better not contain `<space>`!
+note:
+  `"_index"` file format: `<path>,<path>...`
+  This tool will search `SOURCE` path recursive, if it found a `"_index"` file, 
+  then the parent dir will be link to `TARGET`.
+  The name is you specify in `"_index"` file and you can add any more you want.
+  '
+}
+
+if test $# != 2;then usage; exit -1;fi;
 SRC=$1
 TGT=$2
 
